beafc087909aa13139f8f6e274d132eb437ffee5
Justin McCarthy <uvatmvf@gmail.com>
2021-09-27 10:20:39 -0600

    Add Swagger

--- MinApi/Program.cs
+++ MinApi/Program.cs
@@ -2,11 +2,15 @@ using MinApi;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<HelloService>(new HelloService());
 
 var app = builder.Build();
+app.UseSwagger();
 
 app.MapGet("/hello", (HttpContext context, HelloService helloService) => 
     helloService.SayHello(context.Request.Query["name"].ToString()));
+app.UseSwaggerUI();
 
 app.Run();
