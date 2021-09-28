3531aca993770ad20501ee6758bac8ae472a3416
Justin McCarthy <uvatmvf@gmail.com>
2021-09-28 13:17:34 -0600

    Async all the way

--- TodoClient/Data/TodoDataService.cs
+++ TodoClient/Data/TodoDataService.cs
@@ -6,28 +6,31 @@ namespace TodoClient.Data;
 public class TodoDataService
 {
     private readonly string apiEndpoint = "https://localhost:7088/todoitems"; // TODO fix this 
-    
-    public void Delete(TodoItem todo)
-    {
-        var todoRestClient = new RestClient($"{apiEndpoint}/{todo.Id}");
-        var restRequest = new RestRequest(Method.DELETE);
-        todoRestClient.Execute(restRequest);       
-    }
 
-    public IList<TodoItem> GetAll()
-    {
-        var todoRestClient = new RestClient($"{apiEndpoint}");
-        var restRequest = new RestRequest(Method.GET);        
-        var response = todoRestClient.Execute(restRequest);
-        return JsonConvert.DeserializeObject<List<TodoItem>>(response.Content);
-    }
-    public IRestResponse Post(string Title)
-    {
-        var todoRestClient = new RestClient($"{apiEndpoint}");
-        var request = new RestRequest(Method.POST);
-        request.AddJsonBody(JsonConvert.SerializeObject(new TodoItem() { Title = Title }));
-        var response = todoRestClient.Execute(request);
-        return response;
-    }
+    public Task<IRestResponse> Delete(TodoItem todo) =>
+        Task.Run(() =>
+        {
+            var todoRestClient = new RestClient($"{apiEndpoint}/{todo.Id}");
+            var restRequest = new RestRequest(Method.DELETE);
+            return todoRestClient.Execute(restRequest);
+        });
+
+    public Task<IList<TodoItem>> GetAllAsync() =>
+        Task.Run<IList<TodoItem>>(() =>
+        {
+            var todoRestClient = new RestClient($"{apiEndpoint}");
+            var restRequest = new RestRequest(Method.GET);
+            var response = todoRestClient.Execute(restRequest);
+            return JsonConvert.DeserializeObject<List<TodoItem>>(response.Content);
+        });
+    public Task<IRestResponse> Post(string Title) =>
+        Task.Run(() =>
+        {
+            var todoRestClient = new RestClient($"{apiEndpoint}");
+            var request = new RestRequest(Method.POST);
+            request.AddJsonBody(JsonConvert.SerializeObject(new TodoItem() { Title = Title }));
+            var response = todoRestClient.Execute(request);
+            return response;
+        });
 
 }
